db673a82515a69e82d877b67691a2d665ab735a3
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-10-23 17:13:38 +0300

    ORM Fundamentals

--- ORM Fundamentals/src/main/java/org/example/Main.java
+++ ORM Fundamentals/src/main/java/org/example/Main.java
@@ -17,16 +17,27 @@ public class Main {
         Connection connection = getConnection();
 
         EntityManager<User> userEntityManager = new EntityManager<>(connection);
+        userEntityManager.doCreate(User.class);
 
-        User user = new User("Test", "myP@ssword123", 20, LocalDate.now());
+        User user1 = new User("Test1", "myP@ssword123", 21, LocalDate.now());
+        User user2 = new User("Test2", "myP@ssword123", 22, LocalDate.now());
+        User user3 = new User("Test3", "myP@ssword123", 23, LocalDate.now());
 
-        userEntityManager.persist(user);
+        userEntityManager.persist(user1);
+        userEntityManager.persist(user2);
+        userEntityManager.persist(user3);
 
-        user.setUsername("John12");
-        userEntityManager.persist(user);
+        for (User user : userEntityManager.find(User.class)) {
+            System.out.printf("%s - %s ; ", user.getId(), user.getUsername());
+        }
+        System.out.println();
 
-        User johnUserObject = userEntityManager.findFirst(User.class, "`username` LIKE 'John12'");
+        User userToBeDeleted = userEntityManager.findFirst(User.class, "`age` = 22");
 
-        System.out.println(johnUserObject.getUsername());
+        userEntityManager.doDelete(userToBeDeleted);
+
+        for (User user : userEntityManager.find(User.class)) {
+            System.out.printf("%s - %s ; ", user.getId(), user.getUsername());
+        }
     }
 }
