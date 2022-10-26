f7dde0414e07a8c42c8a187c9bd68e4525927dff
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-10-26 22:42:31 +0300

    Exercise Introduction to Hibernate

---
+++ Exercise Introduction to Hibernate/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import entities.Town;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PU_Name");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+    }
+}
