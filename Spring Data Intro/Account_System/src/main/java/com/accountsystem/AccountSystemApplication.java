aec378c8002419ab8d99f5b29c9695a5831ec4eb
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-10-31 22:06:57 +0200

    Spring Data Intro

---
+++ Spring Data Intro/Account_System/src/main/java/com/accountsystem/AccountSystemApplication.java
@@ -0,0 +1,13 @@
+package com.accountsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountSystemApplication.class, args);
+	}
+
+}
---
+++ Spring Data Intro/Account_System/src/test/java/com/accountsystem/AccountSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accountsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
