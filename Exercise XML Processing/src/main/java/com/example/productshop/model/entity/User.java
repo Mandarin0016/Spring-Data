070c8894022bfc5dd64519aefa757be366186710
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-11-15 23:54:21 +0200

    Exercise XML Processing

---
+++ Exercise XML Processing/src/main/java/com/example/productshop/model/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.productshop.model.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    @ManyToMany
+    @JoinTable(
+            name = "users_friends",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "friend_id", referencedColumnName = "id"))
+    private Set<User> friends = new HashSet<>();
+    @OneToMany(mappedBy = "seller", targetEntity = Product.class)
+    private Set<Product> soldProducts = new HashSet<>();
+}
